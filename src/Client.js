a6b6971a081379d91a9d887727f8a99212e8fcf0
Cth1003 <ItsAMeCth@gmail.com>
2016-09-27 20:57:46 +0100

    events

--- src/Client.js
+++ src/Client.js
@@ -7,6 +7,7 @@ Client
 basicly a client,
 yep. a client
 */
+const Message = require('./Message.js');
 const EventEmitter = require('events').EventEmitter;
 const request = require('request');
 const apibase = "https://discordapp.com/api/";
@@ -16,14 +17,37 @@ class Client extends EventEmitter {
 function(token) {
 if (token = null) {
 	console.log("ERROR : You need to add a token.");
-} else {
+} else if (gateway().startsWith('{"url": "wss://gateway.discord.gg')) {
    loginWithToken(token);
+   this.emit("ready")
+	var message = decompressWSMessage(data, flags);
+	var _data = message.d;
+
+	//Events
+	this.emit('any', message);
+	this.emit('debug', message);
+	switch (message.t) {
+		case "MESSAGE_CREATE":
+			this.emit('message', _data.author.username, _data.author.id, _data.channel_id, _data.content, message);
+			return 0;
+		case "MESSAGE_UPDATE":
+			try {
+				this.emit('message_update', message, client._messageCache[_data.channel_id][_data.id], _data);
+			} catch (e) { this.emit("error", message, undefined, _data); }
+			return 0;
+}
+} else {
+	 console.log("ERROR : Gateway isn't ready")
 }
 }
 }
 
-function gateway(token){
-	request(apibase + "?encoding=json&v=4&authorization=" + token, function (error, response, body) {
+function decompressWSMessage(m, f) {
+	f = f || {};
+	return f.binary ? JSON.parse(Zlib.inflateSync(m).toString()) : JSON.parse(m);
+}
+function gateway(){
+	request(apibase + "/gateway?encoding=json&v=4", function (error, response, body) {
     if (!error && response.statusCode == 200) {
     return body;
     } else {
@@ -33,7 +57,15 @@ function gateway(token){
 }
 
 function loginWithToken(token){
-	request.post({url: apibase + 'auth/login', formData: '{token = ' + token + '}'}, function optionalCallback(err, httpResponse, body) {
+request.post({url: apibase + 'auth/login', formData: '{token = ' + token + '}'}, function optionalCallback(err, httpResponse, body) {
+  if (err) {
+    return err;
+  }
+  return body;
+});
+}
+function disconnectWithToken(token){
+request.post({url: apibase + 'auth/logout', formData: '{token = ' + token + '}'}, function optionalCallback(err, httpResponse, body) {
   if (err) {
     return err;
   }
